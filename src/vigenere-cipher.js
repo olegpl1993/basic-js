11d8855e30cc948e019106ec3eee75ce62e6d108
olegpl1993 <olegpl1993@gmail.com>
2022-08-26 20:35:58 +0300

    solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(bool = true) {
+    this.bool = bool;
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  encrypt(txt, key) {
+    if (!txt || !key) throw new Error('Incorrect arguments!');
+    txt = txt.toUpperCase();
+    key = key.toUpperCase();
+    let code = [], final = [];
+    for (let i = 0, n = 0; i < txt.length; i++) {
+      if (this.alphabet.includes(txt[i])) { code.push(this.alphabet.indexOf(key[n]) + this.alphabet.indexOf(txt[i])), n++ }
+      else code.push(txt[i]);
+      if (n >= key.length) { n = 0 };
+    }
+    for (let e = 0; e < code.length; e++) {
+      if (typeof (code[e]) !== "number") final.push(code[e]);
+      for (let i = 0, n = 0; i <= code[e]; i++, n++) {
+        if (n >= 26) n = 0;
+        if (i === code[e]) final.push(this.alphabet[n]);
+      }
+    }
+    return !this.bool ? final.reverse().join('') : final.join('')
+  }
+  decrypt(txt, key) {
+    if (!txt || !key) throw new Error('Incorrect arguments!');
+    txt = txt.toUpperCase();
+    key = key.toUpperCase();
+    let code = [], final = [];
+    for (let i = 0, n = 0; i < txt.length; i++) {
+      if (this.alphabet.includes(txt[i])) { code.push(this.alphabet.indexOf(txt[i]) - this.alphabet.indexOf(key[n])), n++ }
+      else code.push(txt[i]);
+      if (n >= key.length) { n = 0 };
+    }
+    for (let e = 0; e < code.length; e++) {
+      if (typeof (code[e]) !== "number") final.push(code[e]);
+      if (code[e] >= 0) {
+        for (let i = 0, n = 0; i <= code[e]; i++, n++) {
+          if (n >= 26) n = 0;
+          if (i === code[e]) final.push(this.alphabet[n]);
+        }
+      } else {
+        for (let i = 26, n = 26; i >= code[e]; i--, n--) {
+          if (n <= 0) n = 26;
+          if (i === code[e]) final.push(this.alphabet[n]);
+        }
+      }
+    }
+    return !this.bool ? final.reverse().join('') : final.join('')
   }
 }
 
